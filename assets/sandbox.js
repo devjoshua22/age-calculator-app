5339f4729a90966a157dd97827d6b869ded36ffb
devjoshua22 <113951236+devjoshua22@users.noreply.github.com>
2023-08-20 01:55:58 +0100

    Update sandbox.js

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -1,15 +1,17 @@
 //inputs
-const dayInp = document.getElementById("day")
-const monthInp = document.getElementById("months")
-const yearInp = document.getElementById("year")
-console.log(dayInp,monthInp,yearInp)
+const dayInp = document.getElementById("day");
+const monthInp = document.getElementById("months");
+const yearInp = document.getElementById("year");
+console.log(dayInp,monthInp,yearInp);
 
 //outputs
-const dayOup = document.getElementById("DD")
-const monthOup = document.getElementById("MM")
-const yearOup = document.getElementById("YYYY")
-console.log(dayOup,monthOup,yearOup)
+const dayOup = document.getElementById("DD");
+const monthOup = document.getElementById("MM");
+const yearOup = document.getElementById("YYYY");
+console.log(dayOup,monthOup,yearOup);
 
 //form element
-const formEl = document.querySelector("form")
-console.log(formEl)
+const formEl = document.querySelector("form");
+console.log(formEl);
+//handling event
+formEl.addEventListener("submit", handleSubmit);
