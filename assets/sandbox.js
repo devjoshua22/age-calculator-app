1c215d24cd7c082f5fbc257a0542d771c8e07dab
devjoshua22 <113951236+devjoshua22@users.noreply.github.com>
2023-08-20 01:51:55 +0100

    Initial commit

---
+++ assets/sandbox.js
@@ -0,0 +1,15 @@
+//inputs
+const dayInp = document.getElementById("day")
+const monthInp = document.getElementById("months")
+const yearInp = document.getElementById("year")
+console.log(dayInp,monthInp,yearInp)
+
+//outputs
+const dayOup = document.getElementById("DD")
+const monthOup = document.getElementById("MM")
+const yearOup = document.getElementById("YYYY")
+console.log(dayOup,monthOup,yearOup)
+
+//form element
+const formEl = document.querySelector("form")
+console.log(formEl)
