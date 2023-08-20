450afbba057fd231391baa4bf61e2cecce2433b9
devjoshua22 <113951236+devjoshua22@users.noreply.github.com>
2023-08-20 04:04:34 +0100

    fixed the css code

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -14,4 +14,22 @@ console.log(dayOup,monthOup,yearOup);
 const formEl = document.querySelector("form");
 console.log(formEl);
 //handling event
-formEl.addEventListener("submit", handleSubmit);
+//formEl.addEventListener("submit", handleSubmit);
+
+const date = new Date();
+
+let day = date.getDate()
+let month = date.getMonth()+1
+
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+function validation(){
+    const inputs = document.querySelectorAll('input');
+    let validator = true;
+    inputs.forEach((i)=>{
+        const parent = i.parentElement;
+        if(!i.value){
+            i.style.borderColor = "red";
+            parent.querySelector("small").innerText
+        }
+    })
+}
